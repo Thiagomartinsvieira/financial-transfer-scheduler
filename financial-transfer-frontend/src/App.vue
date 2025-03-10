bb71bab4d101539078af35554a385bd61cfa14d3
Thiago Martins Vieira <thiago.devstack@gmail.com>
2025-03-09 23:10:54 -0300

    feat(frontend): initialize Vue.js project

---
+++ financial-transfer-frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="min-h-screen bg-gray-100 flex flex-col">
+    <AppHeader />
+    <main class="flex-1 container mx-auto py-8 px-4">
+      <router-view />
+    </main>
+    <AppFooter />
+  </div>
+</template>
+
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+</script>
